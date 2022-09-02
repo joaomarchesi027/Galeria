3e02792627f631eaa578671e309386061b5cafbf
20211tiimi0389 <joaopmarchesi04@gmail.com>
2022-09-02 11:00:50 -0300

    Initial commit

---
+++ app/src/main/java/tavares/joao/galeria/MainActivity.java
@@ -0,0 +1,14 @@
+package tavares.joao.galeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
