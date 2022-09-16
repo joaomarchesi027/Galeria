844403a8c4f7e5f1d2b7dcfbe54572281d2ff6bf
20211tiimi0389 <joaopmarchesi04@gmail.com>
2022-09-16 11:14:04 -0300

    Passo 6

--- app/src/main/java/tavares/joao/galeria/PhotoActivity.java
+++ app/src/main/java/tavares/joao/galeria/PhotoActivity.java
@@ -1,6 +1,8 @@
 package tavares.joao.galeria;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,12 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 }
