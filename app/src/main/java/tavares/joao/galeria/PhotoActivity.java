5cf56544aa86f5176cc0d3cfca20d909566b560e
joaopedro <joaopmarchesi04@gmail.com>
2022-09-19 18:13:58 -0300

    Correção de bugs

--- app/src/main/java/tavares/joao/galeria/PhotoActivity.java
+++ app/src/main/java/tavares/joao/galeria/PhotoActivity.java
@@ -4,14 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.NinePatch;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class PhotoActivity extends AppCompatActivity {
 
+    String photoPath;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +30,16 @@ public class PhotoActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Intent i = getIntent();
+        photoPath = i.getStringExtra("photo_path");
+
+        NinePatch Utils;
+        Bitmap bitmap = Utils.getBitmap(photoPath);
+        ImageView imPhoto = findViewById(R.id.imPhoto);
+        imPhoto.setImageBitmap(bitmap);
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,7 +59,13 @@ public class PhotoActivity extends AppCompatActivity {
         }
     }
 
-    private void sharePhoto() {
+    void sharePhoto() {
+        // Codigo para cpmpartiilhar a foto
+        Uri photoUri = FileProvider.getUriForFile(PhotoActivity.this, "trindade.daniel.galeria.fileprovider", new File(photoPath));
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.putExtra(Intent.EXTRA_STREAM, photoUri);
+        i.setType("image/jpeg");
+        startActivity(i);
     }
 
 }
